e5f5c1f3528a3461ff012e8f2014636ff72107a3
RevX05@ <117154752+RavikantShinde05@users.noreply.github.com>
2025-12-22 18:15:47 +0530

    Update Arrays.js

--- Arrays.js
+++ Arrays.js
@@ -1,22 +1,23 @@
 // ARRAY DATA TYPE AND METHODS:
 
-// var arr = [1,3,5,11,9];
-// console.log(arr);
+var arr = [1,3,5,11,9];
+console.log(arr);
 
 // ************************************************
 // Array inside an array: Basically creating a matrix.
-// var arr = [
-//     [10,20,30,40],["Ravi", "kartik", "priya", "aarti"]
-// ]
-// console.log(arr[0][1]); 
+var arr = [
+    [10,20,30,40],["Ravi", "kartik", "priya", "aarti"]
+]
+console.log(arr[0][1]); 
 // this is how we can access/console the value inside an array 
 // which is already in an array.
 
 // ************************************************
 // Methods in Arrays:
-// var arr =[10,20,50,60,80]
-// // arr.push(1) only push will insert element from last index of Array
-// // arr.pop(); this will delete the elements from last index of array.
-// console.log(arr);
+var arr =[10,20,50,60,80];
+arr.push(1); //only push will insert element from last index of Array
+arr.pop(); //this will delete the elements from last index of array.
+console.log(arr);
 // for other methods of array use google to practice
+
  

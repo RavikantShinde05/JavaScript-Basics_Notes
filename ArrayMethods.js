0c8e5d7de95e6dd4e16508aa52c589f7e0ba69bd
RevX05@ <117154752+RavikantShinde05@users.noreply.github.com>
2025-12-22 19:15:10 +0530

    Add files via upload

---
+++ ArrayMethods.js
@@ -0,0 +1,35 @@
+// All the array Method which are essentials and used frequently;
+
+array.length();
+// this returns the length of an array;
+
+array.pop();
+// this remove an element of array from last index;
+
+array.push();
+// this adds an element to array from last index;
+
+array.shift();
+// this removes the element of array from first index:
+
+array.unshift();
+// this adds an element to an array from first index/ at the Beginning.
+
+array.slice();
+// this slice out a part/elements of an array into a new array;
+
+array.splice();
+// this will add a new items to an array;
+// And also With clever parameter setting we can remove elements
+// without leaving "holes" in the array.
+
+array.concat();
+// this will creates a new array by merging / concat existing array;
+
+array.toString();
+// this will convert an array to a comma separated string. 
+
+array.at();
+array.join();
+array.delete();
+array.isArray();

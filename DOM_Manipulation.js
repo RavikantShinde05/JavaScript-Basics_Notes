f8428129f56df03d08de5bc9df8699a405c15fb0
RevX05@ <117154752+RavikantShinde05@users.noreply.github.com>
2025-12-22 18:40:25 +0530

    Update DOM_Manipulation.js

--- DOM_Manipulation.js
+++ DOM_Manipulation.js
@@ -1,19 +1,24 @@
 // DOM Manipulation Uses Statements like:
-
+// HTML DOM methods are actions you can perform (on HTML Elements).
 // ************************************************
 // 1. TO FIND;
 
 document.getElementById(id);
 document.getElementsByClassName(className);
+document.getElementsByTagName(tagname);
 // or
 document.querySelector(selector)
 
 // ************************************************
 // 2. TO CHANGE ELEMENT;
 
-element.textContex;
+element.innerHTML = ("newHTML ontent") // change the innerHTML of an element.
 // or
 element.innerText;
+// or 
+element.attribute= ("newValue;") // change the attribute value of an element.
+// or 
+element.style.propertyName = ("newStyle;")  // to change the style property of an element.
 
 
 // ************************************************
@@ -27,3 +32,12 @@ element.style.propertyName;
 
 element.addEventListener(type, listener, UseCapture);
 
+//  ADDING AND DELETING ELEMENTS:
+
+document.createElement("element");
+document.removeChild("element");
+document.appendChild("element");
+document.replaceChild("element");
+document.write("text");
+
+//  and there are many more methods which can be used but not to be memorised cause there are a lot of methods.

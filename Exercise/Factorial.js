a533cdcaca33bc86b56c045603ac45f9645e46be
RevX05@ <117154752+RavikantShinde05@users.noreply.github.com>
2026-01-15 20:35:38 +0530

    Add files via upload

---
+++ Exercise/Factorial.js
@@ -0,0 +1,9 @@
+function factorial(n){
+    // this funciton will help you to find the factorial of the given "n" number "Factorial/n!".
+    let total = 1;
+    for (let i=1; i<=n; i++);
+    total = total*i //another way to put total *= i;
+    return total; 
+};
+
+factorial();
